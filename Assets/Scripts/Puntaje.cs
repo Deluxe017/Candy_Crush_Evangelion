10fe110a24478fc4400967e41d5e65777ab334fd
Delluxxe <jdarrieta5800@misena.edu.co>
2022-09-26 13:59:47 -0500

    Arreglo Escenas

--- Assets/Scripts/Puntaje.cs
+++ Assets/Scripts/Puntaje.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Puntaje : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class Puntaje : MonoBehaviour
 
     private void Update()
     {
-        //puntos += Time.deltaTime;
+
         TextMesh.text = puntos.ToString("0");
     }
 
